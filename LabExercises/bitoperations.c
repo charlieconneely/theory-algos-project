612e770cde1006324f083520380142ce79198b9a
Charlie Conneely <charlieconneely.cc@gmail.com>
2021-02-07 23:50:22 +0000

    Week 1 bit operations exercises finished.

--- LabExercises/bitoperations.c
+++ LabExercises/bitoperations.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 
-int main() {
-  printf("%s\n", "Hello, world!");
+void bin_print(int i)
+{
+  int j = sizeof(int) * 8;
+
+  int k;
+
+  for (j--; j >= 0; j--) {
+    k = ((1 << j) & i) ? 1 : 0;
+    printf("%d", k);
+  }
+}
+
+int main(int argc, char *argv[])
+{
+  int i = 0xf1;
+
+  for (int j = 0; j < 40; j++) {
+    printf("%3d << %2d: ", i, j);
+    bin_print(i << j);
+    printf("\n");
+  }
+
   return 0;
 }
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # theory-algos-project
-Charlie Conneely - G00348887 - Theory of Algorithms Project 20/21 
+Charlie Conneely - G00348887 - Theory of Algorithms Project 20/21 <br></br>
+
+***
+### Resources
+* [Bitwise Operators](https://www.javatpoint.com/bitwise-operator-in-c)
