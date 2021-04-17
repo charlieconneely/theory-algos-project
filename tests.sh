87142ab6ebf822a82f833687f12e0b5895d70950
Charlie Conneely <charlieconneely.cc@gmail.com>
2021-04-17 12:18:41 +0100

    Refactored vars in tests.sh

--- tests.sh
+++ tests.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+testDir="TestFiles"
 # Store all test files in array
-files="$(ls TestFiles)"
+files="$(ls $testDir)"
 
 # Loop through each file and test sha512 output
 for t in ${files[@]}; do
-    echo "Checking file $t"   
-    expec="$(sha512sum TestFiles/$t)"
-    myout="$(./sha512 -p TestFiles/$t)  TestFiles/$t"
+    echo "Checking file $t"
+    expec="$(sha512sum $testDir/$t)"
+    myout="$(./sha512 -p $testDir/$t)  $testDir/$t"
     echo "Expected: " $expec
     echo "Output:   " $myout
     if [[ $myout == $expec ]]; then
